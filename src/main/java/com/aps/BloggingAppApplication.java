54116110be875e3424e24e1bddab12a6dc2d0837
Aman Pal Singh <96203955+Aman-PalSingh@users.noreply.github.com>
2022-07-16 17:24:11 +0530

    Add files via upload

--- src/main/java/com/aps/BloggingAppApplication.java
+++ src/main/java/com/aps/BloggingAppApplication.java
@@ -1,13 +1,20 @@
 package com.aps;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BloggingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingAppApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
