cf522ff28fdf1d7010fcce776157be1503d3123d
Aman Pal Singh <96203955+Aman-PalSingh@users.noreply.github.com>
2022-07-18 15:49:01 +0530

    Add files via upload

--- src/main/java/com/aps/Controllers/CategoryController.java
+++ src/main/java/com/aps/Controllers/CategoryController.java
@@ -2,6 +2,8 @@ package com.aps.Controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,14 +28,14 @@ public class CategoryController {
 
 	// create
 	@PostMapping("/")
-	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
+	public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
 		CategoryDto createdCategory = this.categoryService.createCategory(categoryDto);
 		return new ResponseEntity<CategoryDto>(createdCategory, HttpStatus.CREATED);
 	}
 
 	// update
 	@PutMapping("/{catId}")
-	public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto,
+	public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto,
 			@PathVariable Integer catId) {
 		CategoryDto updatedCategory = this.categoryService.updateCategory(categoryDto, catId);
 		return new ResponseEntity<CategoryDto>(updatedCategory, HttpStatus.OK);
--- src/main/java/com/aps/Payloads/CategoryDto.java
+++ src/main/java/com/aps/Payloads/CategoryDto.java
@@ -1,5 +1,8 @@
 package com.aps.Payloads;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +13,11 @@ import lombok.Setter;
 public class CategoryDto {
 	
 	private Integer id;
+	@NotEmpty
+	@Size(min = 4, message = "Title should aleast be of 4 characters")
 	private String categoryTitle;
+	@NotEmpty
+	@Size(min = 7, message = "Descrition shold be of atleast 7 characters")
 	private String categoryDescription;
 	
 }
